c1138a6bf69c77bd0964535a6219456fe18b6c4e
sjchen <sjchen1981@gmail.com>
2017-12-13 17:49:51 +0800

    update interface

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # esproxy
 web service proxy for es
+
+- Req
+
+```
+http://106.14.26.42:3000/love2io/doc?q=机器&author=gaolinjie&doc=ml-for-humans-zh
+```
+
+- q:查询关键字
+- author:文档作者
+- doc:文档名
+
--- routes/love2io.js
+++ routes/love2io.js
@@ -7,6 +7,8 @@ var router = express.Router();
 var esindex = "cnddu";
 var estype_user = "user";
 var estype_post = "post";
+var estype_booklist = "booklist";
+var estype_tag = "tag";
 
 router.route('/doc')
   .get(function(req, res) {
@@ -43,7 +45,10 @@ router.route('/doc')
 router.route('/site')
   .get(function(req, res) {
       let q = req.query.q;
+      let table = req.query.table;
       console.log("keyword:" + q);
+      console.log("table:" + table);
+      
       async.parallel([ 
           function(callback) {
               let requrl = "http://127.0.0.1:9200/"+esindex+"/"+estype_user+"/_search";
@@ -87,20 +92,67 @@ router.route('/site')
                       callback(null,{'success': 1, 'post':{}});
                   }
               });
-              
+          },function(callback) {
+              let requrl = "http://127.0.0.1:9200/"+esindex+"/"+estype_booklist+"/_search";
+              let options = {
+                  uri: requrl,
+                  method: 'POST',
+                  json: {
+                      "query" : {
+                          "match" : { "title" : q }
+                      }
+                  }
+              };
+              request(options, function(error, response, body) {
+                  console.log(body);
+                  try {
+                      callback(null,{'success': 0, 'booklist':body.hits});
+                  } catch (err) {
+                      console.log("site query err: "+err);
+                      callback(null,{'success': 1, 'booklist':{}});
+                  }
+              });
+          },function(callback) {
+              let requrl = "http://127.0.0.1:9200/"+esindex+"/"+estype_tag+"/_search";
+              let options = {
+                  uri: requrl,
+                  method: 'POST',
+                  json: {
+                      "query" : {
+                          "match" : { "name" : q }
+                      }
+                  }
+              };
+              request(options, function(error, response, body) {
+                  console.log(body);
+                  try {
+                      callback(null,{'success': 0, 'tag':body.hits});
+                  } catch (err) {
+                      console.log("site query err: "+err);
+                      callback(null,{'success': 1, 'tag':{}});
+                  }
+              });
+
           }], function done(err, results) {
               if (err) {
                   throw err;
               }
-              let resp = {"success":0,"user":{},"post":{}};
+              let resp = {"success":0,"user":{},"post":{},"booklist":{},"tag":{}};
               //res.send('async result: '+JSON.stringify(results));
               for (var i = 0; i<results.length; i++) {
-                  if (results[i].post != null) {
+                  if ((results[i].post != null)&&((table == undefined)||(table == "post"))) {
                       resp.post = results[i].post;
                   }
-                  if (results[i].user != null) {
+                  if ((results[i].user != null)&&((table == undefined)||(table == "user"))) {
                       resp.user = results[i].user;
                   }
+                  if ((results[i].booklist != null)&&((table == undefined)||(table == "booklist"))) {
+                      resp.booklist = results[i].booklist;
+                  }
+                  if ((results[i].tag != null)&&((table == undefined)||(table == "tag"))) {
+                      resp.tag = results[i].tag;
+                  }
+
               }
               res.send(resp);
           }
